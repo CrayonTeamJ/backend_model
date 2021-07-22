be10bc9126b1cc89e98d14b6dad4ad69f2494b0a
Somi Kim <somii009@ewhain.net>
2021-07-23 01:24:24 +0900

    finish ffmpeg, upload s3, upload mongodb

--- app.py
+++ app.py
@@ -1,27 +1,37 @@
 from flask import Flask, jsonify, make_response, request
 import requests
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 from flask_pymongo import PyMongo
 from function.to_frame import *
 from detect import *
+from function.s3_control import *
 import ffmpeg
 import shutil
+import config
 
 app = Flask(__name__)
+db = SQLAlchemy()
+migrate = Migrate()
+
+app.config.from_object(config)
+db.init_app(app)
+migrate.init_app(app, db)
 app.config["MONGO_URI"] = "mongodb+srv://team-j-test01:team-j-test01@cluster0.ybzxz.mongodb.net/sample_restaurants?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
 
-@app.route('/to_yolo', methods = ['POST'])
-def user_only():
-    video_info = request.json
+# @app.route('/to_yolo', methods = ['POST'])
+# def user_only():
+#     video_info = request.json
 
-    #data = {'video_pk': pk, 's3_video': video_path}
-    video_pk = request.form['video_pk']
-    video_path = request.form['s3_video']
+#     #data = {'video_pk': pk, 's3_video': video_path}
+#     video_pk = request.form['video_pk']
+#     video_path = request.form['s3_video']
 
-    video_to_Img(video_path)
-    result = run(source='backend_model/data/images')
-    return make_response(jsonify({'pk': video_info['video_pk'], 's3_path': video_info['s3_video']}), 200)
+#     video_to_Img(video_path)
+#     result = run(source='backend_model/data/images')
+#     return make_response(jsonify({'pk': video_info['video_pk'], 's3_path': video_info['s3_video']}), 200)
 
     
 #flask run --host=0.0.0.0 --port=5001
@@ -32,19 +42,34 @@ mongodb_client = PyMongo(app)
 coll = mongodb_client.db.video_files_list
 
 
-@app.route('/to_yolooo', methods = ['POST'])
+@app.route('/to_yolo', methods = ['POST'])
 def run_yolo():
     req_data = request.json
     video_pk = req_data['video_pk']
     video_path = req_data['video_path']
-    dir = 'backend_model/data/{number}/image-%3d.jpg'.format(number=video_pk)
-    # video_to_Img(video_path, video_pk)
-    ffmpeg.input(video_path).filter('fps', fps='1').output(dir, start_number=0, **{'qscale:v': 3}).overwrite_output().run(quiet=True)
-    dir2 = 'backend_model/data/{number}'.format(number=video_pk)
-    return dir2
-    # result = run(source=dir2)
-    # coll.insert({'video_number':video_pk, 'detection_list':result})
+
+    dir = 'data/'+str(video_pk)
+    os.mkdir(dir)
+    output_name = dir+'/image-%3d.jpg'
+    video_to_Img(video_path, video_pk,output_name)
+    # ffmpeg.input(video_path).filter('fps', fps='1').output(output_name, start_number=0, **{'qscale:v': 3}).overwrite_output().run(quiet=True)
+    result = run(source=dir)
+    coll.insert({'video_number':video_pk, 'detection_list':result})
+
+    list_dir = []
+    list_dir = os.listdir(dir)
+    
+    for filename in list_dir:
+        # upload local image files to gcp storage
+        upload_blob_file(dir + '/'+ filename, 'images/'+ str(video_pk)+ '/'+ filename)
+        
+        # delete local image files
+    
+    shutil.rmtree(dir)
+
+
+    return result
+
     # 아마존 s3에 업로드하는 코드 한줄(dir2)
-    # ## shutil.rmtree(dir2)
     # return make_response(jsonify({'Result': 'Success', 'video_pk': video_pk}), 200)
 
--- function/to_frame.py
+++ function/to_frame.py
@@ -2,10 +2,10 @@ import subprocess
 import os
 import ffmpeg
 
-def video_to_Img(video_path, video_pk):
+def video_to_Img(video_path, video_pk, output_name):
     
     try:
-        ffmpeg.input(video_path).filter('fps', fps='1').output('backend_model/data/{number}/image-%3d.jpg'.format(number=video_pk), start_number=0, **{'qscale:v': 2}).overwrite_output().run(quiet=True)
+        ffmpeg.input(video_path).filter('fps', fps='1').output(output_name, start_number=0, **{'qscale:v': 2}).overwrite_output().run(quiet=True)
     except ffmpeg.Error as e:
         print('stdout:', e.stdout.decode('utf8'))
         print('stderr:', e.stderr.decode('utf8'))
