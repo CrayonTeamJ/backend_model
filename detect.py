4ae98dfdc8d89b6ac7b6b1a5f97281123c968779
Hayoung Lim <hayounglim@minerva.kgi.edu>
2021-07-22 16:20:41 -0700

    chore: remove 'parse-opt' commands

--- detect.py
+++ detect.py
@@ -175,29 +175,6 @@ def run(weights='best_4.pt',  # model.pt path(s)
                 cv2.imshow(str(p), im0)
                 cv2.waitKey(1)  # 1 millisecond
 
-            # Save results (image with detections)
-    #         if save_img:
-    #             if dataset.mode == 'image':
-    #                 cv2.imwrite(save_path, im0)
-    #             else:  # 'video' or 'stream'
-    #                 if vid_path[i] != save_path:  # new video
-    #                     vid_path[i] = save_path
-    #                     if isinstance(vid_writer[i], cv2.VideoWriter):
-    #                         vid_writer[i].release()  # release previous video writer
-    #                     if vid_cap:  # video
-    #                         fps = vid_cap.get(cv2.CAP_PROP_FPS)
-    #                         w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    #                         h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    #                     else:  # stream
-    #                         fps, w, h = 30, im0.shape[1], im0.shape[0]
-    #                         save_path += '.mp4'
-    #                     vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
-    #                 vid_writer[i].write(im0)
-
-    # if save_txt or save_img:
-    #     s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
-    #     print(f"Results saved to {save_dir}{s}")
-
     if update:
         strip_optimizer(weights)  # update model (to fix SourceChangeWarning)
 
@@ -210,43 +187,3 @@ def run(weights='best_4.pt',  # model.pt path(s)
 
     # with open('result.json', 'w', encoding='UTF-8') as outfile:
     #     json.dump(output, outfile, indent=4, ensure_ascii=False)
-
-def parse_opt():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='data/images', help='file/dir/URL/glob, 0 for webcam')
-    parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
-    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='show results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
-    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--visualize', action='store_true', help='visualize features')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
-    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
-    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
-    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
-    opt = parser.parse_args()
-    return opt
-
-
-def main(opt):
-    print(colorstr('detect: ') + ', '.join(f'{k}={v}' for k, v in vars(opt).items()))
-    check_requirements(exclude=('tensorboard', 'thop'))
-    run(**vars(opt))
-
-
-if __name__ == "__main__":
-    opt = parse_opt()
-    main(opt)
