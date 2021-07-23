8ecefd676e7752049c1693539925547ba0ba2bb7
Hayoung Lim <hayounglim@minerva.kgi.edu>
2021-07-23 00:16:08 -0700

    chore: change yolo threshold

--- detect.py
+++ detect.py
@@ -30,7 +30,7 @@ from collections import OrderedDict
 def run(weights='best_4.pt',  # model.pt path(s)
         source='data/images',  # file/dir/URL/glob, 0 for webcam
         imgsz=640,  # inference size (pixels)
-        conf_thres=0.25,  # confidence threshold
+        conf_thres=0.65,  # confidence threshold
         iou_thres=0.45,  # NMS IOU threshold
         max_det=1000,  # maximum detections per image
         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
